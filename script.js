18825e52138e2a726f1d4f5b214cc17ffe2755f4
stinkjunk <magnuskrahl2@gmail.com>
2025-09-25 13:37:25 +0200

    desktop titel opdateret

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 const invSwitch = document.querySelector(".invSwitch");
 const writingCursors = document.querySelectorAll(".writingCursor");
+const images = document.querySelectorAll("img");
+const scrollDown = document.querySelector("#scrollDown");
+const pageTitle = document.querySelector("#pageTitle");
 
 window.addEventListener("resize", () => {
   updateSpacing();
@@ -20,23 +23,44 @@ function updateSpacing() {
 }
 
 writingCursors.forEach((cursor) => {
-  //   cursor.style.opacity = 0;
-  //   cursor.innerHTML = "|";
-  //   setTimeout(() => {
-  //     cursor.style.opacity = 1;
-  //   }, 1000);
   cursorBlink(cursor);
 });
 
-function cursorBlink(cursor) {
+images.forEach((image) => {
+  image.title = "Tryk på mig for at se det ubehandlede billede";
+
+  image.addEventListener("click", () => {
+    image.classList.toggle("processed");
+    if (image.classList.contains("processed")) {
+      // image.title = "Tryk på mig for at se det uforarbejdede billede";
+      image.title = "Tryk på mig for at se det ubehandlede billede";
+    } else {
+      // image.title = "Tryk på mig for at slå forarbejdning til";
+      image.title = "Tryk på mig for at slå filter til";
+    }
+  });
+});
+
+cursorBlink(scrollDown, "↴");
+
+function cursorBlink(cursor, symbol = "_") {
   cursor.style.opacity = cursor.style.opacity === "0" ? "1" : "0";
-  cursor.innerHTML = "_";
-  setTimeout(() => cursorBlink(cursor), 500);
+  cursor.innerHTML = symbol;
+  setTimeout(() => cursorBlink(cursor, symbol), 500);
 }
 
 updateSpacing();
 
-// headerspacer.offsetHeight;
+window.addEventListener("scroll", scrollUpdater);
 
-//i toppen ved refresh/pageload
-window.scrollTo(0, 0);
+function scrollUpdater() {
+  const percent = 0.6;
+  const scrollTop = window.scrollY;
+  const viewPortHeight = window.innerHeight;
+  const viewPortPercent = viewPortHeight * percent;
+  if (scrollTop > viewPortPercent) {
+    pageTitle.classList.add("invis");
+  } else {
+    pageTitle.classList.remove("invis");
+  }
+}
